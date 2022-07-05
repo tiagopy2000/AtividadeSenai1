555ca6544aea7da997bc53597cdbb020d825386e
tiagopy2000 <tiagosocial2001@gmail.com>
2022-07-05 16:52:14 -0300

    Código e Diagrama

---
+++ Atividade1.js
@@ -0,0 +1,24 @@
+let pecaPeso = 0;
+let nomePeca = 0;
+let  numpecas= 0;
+
+pecaPeso = prompt ("Coloque o peso da peça desejada");
+
+if (pecaPeso > 100 ){
+  nomePeca = prompt ("Qual nome deseja cadastra na peça?");
+
+  if(nomePeca.length >=3 ){
+    numpecas = prompt ("Coloque a quantidade de peças para caixa");
+
+      if(numpecas >= 10){
+        console.log ("a caixa já esta cheia");
+
+      }else{
+         console.log ("Caixa cadastrada com sucesso");
+      }
+  } else{
+    console.log("o nome cadatrado é muito pequeno ");
+  }
+} else{
+   console.log("a peça não tem o peso minimo para cadastro no sistema")
+}
